539e5b16b18b192b4d24e83bceb2703f68a1c4fb
TimNysetvold <tim.nysetvold@gmail.com>
2024-05-02 18:25:59 +0000

    Finish

--- app.rb
+++ app.rb
@@ -2,8 +2,63 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do 
+
+  moves = ["rock", "paper","scissors"]
+  @comp_move = moves.sample
+  @my_move = "rock"
+
+  @outcome = "lost."
+
+  if (@comp_move=="rock")
+    @outcome="tied."
+  elsif (@comp_move == "scissors")
+    @outcome = "won!"
+  else
+    @outcome = "lost."
+  end
+
+  erb(:movelook)
+
+end
+
+get("/paper") do 
+  moves = ["rock", "paper","scissors"]
+  @my_move = "paper"
+  @comp_move = moves.sample
+
+  @outcome = "lost."
+
+  if (@comp_move=="paper")
+    @outcome="tied."
+  elsif (@comp_move == "rock")
+    @outcome = "won!"
+  else
+    @outcome = "lost."
+  end
+
+  erb(:movelook)
+end
+
+get("/scissors") do
+
+  moves = ["rock", "paper","scissors"]
+  @comp_move = moves.sample
+  @my_move = "scissors"
+
+  @outcome = "lost."
+
+  if (@comp_move=="scissors")
+    @outcome="tied."
+  elsif (@comp_move == "paper")
+    @outcome = "won!"
+  else
+    @outcome = "lost."
+  end
+
+  erb(:movelook)
+
 end
